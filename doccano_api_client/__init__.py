7c2bd479e1209392a5c445aa229a1f667dcda3ed
Jessica Ojo <jessicaojo19@gmail.com>
2022-06-15 08:17:25 +0100

    customising to fit okra's current doccano version

--- doccano_api_client/__init__.py
+++ doccano_api_client/__init__.py
@@ -938,11 +938,11 @@ class DoccanoClient(_Router):
         arr_response = []
         for username, rolename in zip(usernames, roles):
             user = list(filter(lambda user_info: user_info["username"] == username, res_users))
-            assert len(user) >= 1, "username {username} not found".format(username=username)
+            assert len(user) >= 1, f"username {username} not found"
             user = user[0]
 
-            role = list(filter(lambda role_info: role_info["rolename"] == rolename, res_roles))
-            assert len(role) >= 1, "rolename {rolename} not found".format(rolename=rolename)
+            role = list(filter(lambda role_info: role_info["name"] == rolename, res_roles))
+            assert len(role) >= 1, f"rolename {rolename} not found"
             role = role[0]
 
             arr_response.append(
