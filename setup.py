71fe5c411d6c2b9d611bd91aea6cea252d6fe7fe
Jessica Ojo <jessicaojo19@gmail.com>
2022-06-21 11:53:16 +0100

    package build updates

--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
-from setuptools import setup
+import setuptools
 
 base_packages = [
     "requests",
     "dataclasses>=0.7,<1; python_version<'3.7'",
-    "dataclasses-json>=0.5.0,<1.0.0",
-]
+    "dataclasses-json>=0.5.0,<1.0.0"]
 
 dev_packages = [
     "black>=19.3b0",
@@ -17,32 +16,30 @@ dev_packages = [
     "pre-commit>=2.2.0",
     "types-dataclasses>=0.1.3,<1; python_version<'3.7'",
     "types-requests>=0.1.8,<1",
-    "pytest>=6.1.2,<7",
-]
+    "pytest>=6.1.2,<7"]
 
-setup(
+setuptools.setup(
     name="okra-doccano-client",
-    version="1.1.0",  # Beta Client Tested for compatibility against Doccano >=1.5.0, <=1.5.5
+    version="1.0",
     setup_requires=["setuptools_scm"],
     description="A simple client wrapper customised for okra doccano API.",
-    long_description="This simple API wrapper forked from doccano customised for okra that allows users to easily get data \
-            from and send data to a doccano instance.",
-    keywords="forked doccano api client wrapper customized for okra",
+    long_description="This simple API wrapper forked from doccano customised for okra that allows users to easily get"
+                     " data from and send data to a doccano instance.",
     url="https://github.com/okraHQ/okra-ai-doccano-client",
-    author="Andrew Parsons",
-    author_email="parsonsandrew1@gmail.com",
+    author="Okra Ds Team",
+    author_email="chidera@okra.ng",
     license="MIT",
     packages=["doccano_api_client"],
     install_requires=base_packages,
     extras_require={"dev": dev_packages},
     zip_safe=False,
     classifiers=[
-        "License :: OSI Approved :: MIT License',"
+        "License :: OSI Approved :: MIT License",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: Implementation :: CPython",
-        "Programming Language :: Python :: Implementation :: PyPy",
-    ],
+        "Programming Language :: Python :: Implementation :: PyPy"
+    ]
 )
